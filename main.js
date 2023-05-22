e35a2f796154218dde1c583ae77c774df39b8f83
aberrantian <aberrantian@gmail.com>
2023-05-22 21:56:39 +0100

    create pseudocode

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 console.log("hello world");
 
 document.body.style.backgroundColor = "#303446"; // dark theme
+
+/*
+let expression = expGen()
+let solution = expSolve(expression)
+
+console.log(expression)
+console.log(solution)
+*/
